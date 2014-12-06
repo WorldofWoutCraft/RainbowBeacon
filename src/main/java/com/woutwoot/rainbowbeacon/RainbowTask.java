00938eb48c8de70bc8ce7d44f3e83900bb641cc1
woutwoot <woutwoot13@gmail.com>
2014-12-06 13:10:11 +0100

    Save over restarts

--- src/main/java/com/woutwoot/rainbowbeacon/RainbowTask.java
+++ src/main/java/com/woutwoot/rainbowbeacon/RainbowTask.java
@@ -1,5 +1,9 @@
 package com.woutwoot.rainbowbeacon;
 
+import org.bukkit.Material;
+import org.bukkit.block.Beacon;
+import org.bukkit.block.BlockFace;
+
 /**
  * Created by Wout on 29/11/2014.
  */
@@ -9,6 +13,12 @@ public class RainbowTask implements Runnable {
     public void run() {
         for (RainbowBlock b : RainbowBeacon.getInstance().getRainbowBlocks()) {
             b.nextColor();
+            if (b.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() == Material.BEACON) {
+                Beacon beacon = (Beacon) b.getLocation().getBlock().getRelative(BlockFace.DOWN).getState();
+                if (beacon != null) {
+                    beacon.update();
+                }
+            }
         }
     }
 }
